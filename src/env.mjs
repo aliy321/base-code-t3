709f714136554c8d537491439176520e727d2b4c
Aliy Akhbar <aliyakhbar@gmail.com>
2023-10-22 20:31:33 +0800

    tweak env

--- src/env.mjs
+++ src/env.mjs
@@ -19,7 +19,6 @@ export const env = createEnv({
     //     "You forgot to change the default URL"
     //   ),
     DATABASE_URL: z.string().url(),
-    VERCEL_URL: z.string().optional(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -32,6 +31,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_APP_URL: z.string().url().optional(),
   },
 
   /**
@@ -40,7 +40,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
-    VERCEL_URL: process.env.VERCEL_URL,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
