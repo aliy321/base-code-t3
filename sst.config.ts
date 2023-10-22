3be75c4c7609bc9554fca9a1fb9a0d71677baed5
Aliy Akhbar <aliyakhbar@gmail.com>
2023-10-22 18:40:01 +0800

    test env again

--- sst.config.ts
+++ sst.config.ts
@@ -14,17 +14,6 @@ export default {
         edge: true,
         bind: [],
         environment: {
-          NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL!,
-          NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL!,
-          NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL!,
-          NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL!,
-          NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!,
-          CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY!,
-          NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL!,
-          RESEND_API_KEY: process.env.RESEND_API_KEY!,
-          NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!,
-          STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY!,
-          STRIPE_PROD: process.env.STRIPE_PROD!,
           DATABASE_URL: process.env.DATABASE_URL!,
         },
       });
