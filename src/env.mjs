65245803a086add26dca6df65485eb161d3f6f6a
Aliy Akhbar <aliyakhbar@gmail.com>
2023-10-22 18:35:01 +0800

    test env

--- src/env.mjs
+++ src/env.mjs
@@ -1,9 +1,5 @@
-import {
-  createEnv
-} from "@t3-oss/env-nextjs";
-import {
-  z
-} from "zod";
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 export const env = createEnv({
   /**
@@ -30,13 +26,6 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
-    // NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string(),
-    // NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string(),
-    // NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string(),
-    // NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string(),
-    // NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
-    // NEXT_PUBLIC_APP_URL: z.string(),
-    // NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string(),
   },
 
   /**
@@ -44,9 +33,9 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
