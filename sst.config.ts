413af5890ddef42ad8f2f5d66e6a043e88bf1b28
Aliy Akhbar <aliyakhbar@gmail.com>
2023-10-22 19:09:44 +0800

    tweak sst

--- sst.config.ts
+++ sst.config.ts
@@ -1,4 +1,4 @@
-import type { SSTConfig } from "sst";
+import { type SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 
 export default {
@@ -11,8 +11,6 @@ export default {
   stacks(app) {
     app.stack(function Site({ stack }) {
       const site = new NextjsSite(stack, "site", {
-        edge: true,
-        bind: [],
         environment: {
           DATABASE_URL: process.env.DATABASE_URL!,
         },
@@ -23,4 +21,4 @@ export default {
       });
     });
   },
-} as SSTConfig;
+} satisfies SSTConfig;
