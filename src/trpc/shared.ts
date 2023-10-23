3aba72a8d8bc92c18363b447beba7657be348088
Aliy Akhbar <aliyakhbar@gmail.com>
2023-10-23 19:55:14 +0800

    Added new env key

--- src/trpc/shared.ts
+++ src/trpc/shared.ts
@@ -7,7 +7,7 @@ export const transformer = superjson;
 
 function getBaseUrl() {
   if (typeof window !== "undefined") return "";
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (process.env.NEXT_PUBLIC_APP_URL) return `${process.env.NEXT_PUBLIC_APP_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
